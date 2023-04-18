9b8c1da8b2be32b7c9f9e0894e9cf6fde23266dd
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2023-04-18 16:27:30 +0200

    documentation

--- data/study search/database search/processed/preparation for abstract screening/README.md
+++ data/study search/database search/processed/preparation for abstract screening/README.md
@@ -9,7 +9,7 @@ Here we outline the steps which were conducted on the data from the initial data
 
 2. We then applied the script [`extracting_URLs_script.R`](https://github.com/META-CMP/data/blob/main/data/study%20search/database%20search/processed/preparation%20for%20abstract%20screening/retrieving%20missing%20or%20incomplete%20abstracts/extracting_URLs_script.R) to the merged and duplicate-free dataset to extract the URLs (or, if no URL exists, the titles) of those 5572 [entries](https://github.com/META-CMP/data/blob/main/data/study%20search/database%20search/processed/preparation%20for%20abstract%20screening/retrieving%20missing%20or%20incomplete%20abstracts/entries_without_complete_abstracts.csv) that have no abstract or only an incomplete abstract (Google Scholar only extracts abbreviated abstracts and both Google Scholar and EconLit sometimes have missing abstracts). We then manually searched for these entries and retrieved their abstracts, if available, from the internet.
 
-# Retrieving missing abstracts when only titles are available
+## Retrieving missing abstracts when only titles are available
 
 This section describes the steps taken to retrieve missing abstracts for the 73 Zotero entries for which only titles were available. We conducted them on February 21st and 22nd in 2023.
 
@@ -22,16 +22,46 @@ This section describes the steps taken to retrieve missing abstracts for the 73 
   
 - Option two and three of the last point were also conducted if the title of the paper seemed to fit our research question at a very basic level and none of the previous steps was successful. 
 
+## Retrieving missing abstracts when urls are available
+
+Based on the URLs and titles from entries without complete abstracts, a student assistant collected abstracts from the websites of the bibliographic entries, where possible. 
+
+### How to merge these completed entries with the already complete entries?
+
+#### 1st Step
+
+First, we prepared a dataset, that contains those entries from the initial duplicate-free dataset that are not in "entries_without_complete_abstracts.csv". We added a function to `extracting_URLs_script.R` that did this. The file was stored as `entries_complete_abstracts.csv`.
+
+#### 2nd Step
+
+Afterward, we imported the initial "merged_EL_GS_no_duplicates.ris" into Zotero to make Zotero know the keys. Subsequently, we imported our ris-file that we retrieved based on the 73 titles of the entries that had no URLs. We removed the remaining duplicates and entries with missing abstracts. We exported this file to the GitHub repo as `abstracts_from_titles.ris` and `abstracts_from_titles.csv`.
+
+#### 3rd Step
+
+We imported the initial "merged_EL_GS_no_duplicates.ris" into Zotero to make Zotero know the keys. Afterwards, we created an empty new collection in Zotero. Next, we imported the ris-files of the student assistant one by one, and move them as a subcollection into the empty collection (drag and drop). Subsequently, we deleted the "merged_EL_GS_no_duplicates" collection (with items!) from Zotero. This procedure should ensure that Zotero would have resolved any "Key" conflicts between the original dataset and the ris-files from the student assistant. In a last step, we removed duplicated item directly in Zotero.
+
+The total collection of duplicate free student ris-files is saved as `abstracts_from_URLs.ris` and `abstracts_from_URLs.csv` in the folder "retrieving missing or incomplete abstracts". 
+
 For 24 studies, this procedure provided us with the corresponding abstracts for the respective papers. 
 
-3. Finally, we merged 
 
-...
+3. Our search strategy is supplemented by a screening of the existing meta-studies that are relevant for our research interest. From these meta-studies we add all studies to our database that were not yet covered by our search.
 
-to retrieve our final dataset for title and abstract screening. 
-
+Therefore, we manually collected all 43 Studies of Nguyen (2021) and 45 Studies of Nguyen (2020) and read the data of Havranek et al (2013) (available at: http://meta-analysis.cz/lags/lags.zip) and Rusnak et al (2013) (http://meta-analysis.cz/price_puzzle/puzzle.xls) directly into R using the `code_to_read_in_data.R` script. 
 
 
+4. Finally, we merged 
+
+1. `entries_complete_abstracts.csv`
+2. `abstracts_from_titles.csv`
+3. `abstracts_from_URLs.csv`
+4. `havranek_2013.csv`
+5. `Nguyen_2021_2020.csv`
 
 
-# https://github.com/META-CMP/data/tree/merging_retrieved_abstracts/data/study%20search/database%20search/processed/preparation%20for%20abstract%20screening in dem file alles ergänzen 
+to retrieve our dataset for a last round of final checks conducted in [`merge_all_files.R`](). In this final checks we randomly screened for additional available abstracts when the abstract note was "NA", checked the abstracts for remaining "..." quotes, and conducted two additional rounds of duplicates removal using the app of the "revtools" Package in R. Afterward, we remove the entries without abstract or title and save the final dataset for abstract screening as `unlabelled_full_sample.csv`.
+
+
+The resulting literature data base [containing 10714 entries] will be screened based on title and abstract.
+
+
---
+++ data/study search/database search/processed/preparation for abstract screening/studies from other meta studies/code_to_read_in_data.R
@@ -0,0 +1,148 @@
+library(readr)
+
+merged_EL_GS_no_duplicates <- read_csv("data/study search/database search/processed/preparation for abstract screening/merging of EL and GS results and duplicate removal/merged_EL_GS_no_duplicates.csv")
+
+Nguyen_2021 <- read_csv("Nguyen_2021.csv")
+Nguyen_2020 <- read_csv("Nguyen_2020.csv")
+
+#De grauwe do not list their studies, the others do not state their search strategy.
+
+# Load the readxl package
+library(readxl)
+
+
+# Read in the third sheet of the Excel file
+Havranek_2013 <- read_excel("Havranek_2013.xls", sheet = "studies")
+Rusnak_2013 <- read_excel("Rusnak_2013.xls", sheet = "studies")
+
+# Havranek_2013 and Rusnak use the exact same sample of studies
+sum(Rusnak_2013$Title==Havranek_2013$Title)
+
+
+
+
+
+# ########################## NGUYEN 2020
+
+
+# Create data frames
+df1 <- data.frame(Nguyen_2020 %>% select(`Publication Year`,Author,Title))
+df2 <- data.frame(merged_EL_GS_no_duplicates %>% select(`Publication Year`,Author,Title))
+
+# Create single character string for information we would like to investigate
+string1<-paste(df1$Publication.Year,df1$Author,df1$Title)
+string2<-paste(df2$Publication.Year,df2$Author,df2$Title)
+
+# It creates a matrix with the Standard Levenshtein distance between the name fields of both sources
+dist.name_full<-adist(string1,string2, partial = TRUE, ignore.case = TRUE)
+
+# To name the columns of this matrix with the respective names of our dataset
+colnames(dist.name_full)<-string2
+
+# get the best match by retrieving the minimum 
+min_dist_full<-apply(dist.name_full, 1, min)
+
+# get the study information for the closest match
+min_cols_full <- apply(dist.name_full, 1, function(x) colnames(dist.name_full)[which(x == min(x))])
+
+# list to data frame
+data_frame_author <- as.data.frame(do.call(rbind, min_cols_full))
+
+# Generate data frame for orignial string from the other meta studies, the distance measure and the string of our study 
+matches <- cbind(string1,min_dist_full,data_frame_author)
+colnames(matches)[3:4]<-c("Our_sample","Our_sample_option_2")
+
+# check these observations which seem different
+LOOK<-matches[min_dist_full > 26, ]
+
+
+########################## NGUYEN 2021
+
+#Alternative by combininig year author and title
+df1 <- data.frame(Nguyen_2021 %>% select(`Publication Year`,Author,Title))
+df2 <- data.frame(merged_EL_GS_no_duplicates %>% select(`Publication Year`,Author,Title))
+
+
+# Create single character string for information we would like to investigate
+string1<-paste(df1$Publication.Year,df1$Author,df1$Title)
+string2<-paste(df2$Publication.Year,df2$Author,df2$Title)
+
+# It creates a matrix with the Standard Levenshtein distance between the name fields of both sources
+dist.name_full<-adist(string1,string2, partial = TRUE, ignore.case = TRUE)
+
+# To name the columns of this matrix with the respective names of our dataset
+colnames(dist.name_full)<-string2
+
+# get the best match by retrieving the minimum 
+min_dist_full<-apply(dist.name_full, 1, min)
+
+# get the study information for the closest match
+min_cols_full <- apply(dist.name_full, 1, function(x) colnames(dist.name_full)[which(x == min(x))])
+
+# list to data frame
+data_frame_author <- as.data.frame(do.call(rbind, min_cols_full))
+
+# Generate data frame for orignial string from the other meta studies, the distance measure and the string of our study 
+matches <- cbind(string1,min_dist_full,data_frame_author)
+colnames(matches)[3:4]<-c("Our_sample","Our_sample_option_2")
+
+# check these observations which seem different
+LOOK1<-matches[min_dist_full > 26, ]
+
+
+
+
+
+########################## Rusnak_2013
+
+#Alternative by combininig year author and title
+df1 <- data.frame(Rusnak_2013 %>% select(Year,Study,Title))
+df2 <- data.frame(merged_EL_GS_no_duplicates %>% select(`Publication Year`,Author,Title))
+
+
+df1$Study<-vapply(strsplit(df1$Study,split = " [(]"), `[`, 1, FUN.VALUE=character(1))
+
+# Create single character string for information we would like to investigate
+string1<-paste(df1$Year,df1$Study,df1$Title)
+string2<-paste(df2$Publication.Year,df2$Author,df2$Title)
+
+# It creates a matrix with the Standard Levenshtein distance between the name fields of both sources
+dist.name_full<-adist(string1,string2, partial = TRUE, ignore.case = TRUE)
+
+# To name the columns of this matrix with the respective names of our dataset
+colnames(dist.name_full)<-string2
+
+# get the best match by retrieving the minimum 
+min_dist_full<-apply(dist.name_full, 1, min)
+
+# get the study information for the closest match
+min_cols_full <- apply(dist.name_full, 1, function(x) colnames(dist.name_full)[which(x == min(x))])
+
+# list to data frame
+data_frame_author <- as.data.frame(do.call(rbind, min_cols_full))
+
+# Generate data frame for orignial string from the other meta studies, the distance measure and the string of our study 
+matches <- cbind(string1,min_dist_full,data_frame_author)
+colnames(matches)[3:4]<-c("Our_sample","Our_sample_option_2")
+
+# check these observations which seem different
+LOOK2<-matches[min_dist_full > 25, ]
+
+# 2010 Forni and Gambetti The dynamic effects of monetary policy: A structural factor model approach they have a newer study ours is from 2008
+# 2004 EFN Monetary transmission in acceding countries # ONLY APPENDIX FOUND AND NO ABSTRACT
+# 1997 Bernanke et al. Systematic Monetary Policy and the Effects of Oil Price Shocks (HERE WE ONLY HAVE THE COMMENT BUT NOT THE ORIGINAL STUDY) - NO ABSTRACT
+# 2009 Borys et al. The effects of monetary policy in the Czech Republic: an empirical study - MAYBE THERE EXCISTS A NEWER STUDY we only have 2008
+# 2000 Shioji Identifying Monetary Policy Shocks in Japan
+# 2010 Lange Regime-switching monetary policy in Canada
+# 2009 Peersman and Straub Technology Shocks and Robust Sign Restrictions in a Euro Area SVAR - maybe newer version available (ours is from 2004)
+# 2010 Banbura et al. Large Bayesian VARs
+# 2008 Cespedes et al. Monetary Policy, Inflation and the Level of Economic Activity in Brazil after the Real Plan: Stylized Facts from SVAR Models (ours is from 2005)
+# 2008 Andries Monetary policy transmission mechanism in Romania - a VAR approach
+# 1992 Eichenbaum Comment on 'Interpreting the macroeconomic time series facts: The effects of monetary policy' NO ABSTRACT
+# 1998 Leeper, Sims and Zha What does monetary policy do?
+# 2008 Mertens Deposit rate ceilings and monetary transmission in the US
+
+
+
+LOOK$covered_in_LOOK1<-is.element(LOOK$string1,LOOK1$string1)
+MISSING<-rbind(LOOK1,LOOK %>% filter(covered_in_LOOK1==FALSE) %>% select(-covered_in_LOOK1,-V3,-V4))

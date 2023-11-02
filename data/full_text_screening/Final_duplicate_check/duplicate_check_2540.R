42c2496839ca38d6bd1a65e489d3dcc77826a819
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2023-11-02 09:53:20 +0100

    initial coding of XNPIMZ6E

--- data/full_text_screening/Final_duplicate_check/duplicate_check_2540.R
+++ data/full_text_screening/Final_duplicate_check/duplicate_check_2540.R
@@ -42,6 +42,6 @@ screen_duplicates(as.data.frame(df))
 
 df<-read.csv("duplicate_free_final.csv")
 
-test<-df %>% filter(grepl("Boivin",author, fixed = TRUE))
+test<-df %>% filter(grepl("Agenor",author, fixed = TRUE))
 
 

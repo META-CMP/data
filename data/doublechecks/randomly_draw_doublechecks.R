9d6a3bef397feed3518861bdb50372407ac41ada
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2023-12-06 14:35:16 +0100

    initial coding of CLC34NH8

--- data/doublechecks/randomly_draw_doublechecks.R
+++ data/doublechecks/randomly_draw_doublechecks.R
@@ -2,21 +2,29 @@ library(dplyr)
 library(tidyr)
 
 
-setwd("~/data/data/snowballing")
-getwd()
+# setwd("~/data/data/snowballing")
+# getwd()
 
 
 
-## read in xlsx files after the full text screening and the adjustment of the bibliographic information.
-library(readxl)
-file.list <- 1:26
-df.list <- lapply(file.list, function(x) read_excel(paste0("study_set_",x,".xlsx")))
+## read in xlsx files after the full text screening and the adjustment of the bibliographic information, do this only once to create r file.
+# library(readxl)
+# file.list <- 1:26
+# df.list <- lapply(file.list, function(x) read_excel(paste0("study_set_",x,".xlsx")))
+# 
+# 
+# library(dplyr)
+# df_full_text <- bind_rows(df.list, .id = "id")
+# adjust type of doublecheck column
+# df_full_text$doublecheck<-ifelse(df_full_text$doublecheck!=1 | is.na(df_full_text$doublecheck),FALSE,TRUE)
+#
+# setwd("~/data/data/doublechecks")
+# saveRDS(df_full_text,"df_full_text")
 
+setwd("~/data/data/doublechecks")
 
-library(dplyr)
-df_full_text <- bind_rows(df.list, .id = "id")
-df_full_text$doublecheck<-ifelse(df_full_text$doublecheck!=1 | is.na(df_full_text$doublecheck),FALSE,TRUE)
-
+#load data 
+df_full_text<-readRDS("df_full_text")
 
 
 ################## Adapt these three settings:
@@ -32,12 +40,19 @@ doublecheck_only<-FALSE
 #### Put in the number of studies you want to draw: 
 n_doublecheck<-10
 
+#### round of double check draw per resercher
+round<-1
 
+
+sum(df_full_text$included, na.rm = TRUE)
 
 # draw random sample
-random_sample<-df_full_text %>% filter(rid1!=researcher & is.na(rid2) & doublecheck==doublecheck_only) %>% # filter to only consider relevant entries
+random_sample<-df_full_text %>% filter(rid1!=researcher & is.na(rid2) & doublecheck==doublecheck_only & included==1) %>% # filter to only consider relevant entries
                                 slice_sample(n=n_doublecheck, replace = FALSE) # drawn n observations
 
 
 
+df_full_text<-setdiff(df_full_text,random_sample)
 
+saveRDS(df_full_text,"df_full_text")
+saveRDS(random_sample,paste0("random_sample_",researcher,round))

41ff4d50396595eb189181d4ed3e7d252fc7c67a
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2023-12-15 16:27:27 +0100

    assign doublechecks

--- data/doublechecks/randomly_draw_doublechecks.R
+++ data/doublechecks/randomly_draw_doublechecks.R
@@ -1,58 +1,77 @@
-library(dplyr)
 library(tidyr)
 
+#################################### run this only once to set up the doublecheck files #########################
 
+# # Set the working directory to "~/data/data/snowballing"
 # setwd("~/data/data/snowballing")
-# getwd()
 # 
 # 
-# 
-# ## read in xlsx files after the full text screening and the adjustment of the bibliographic information, do this only once to create r file.
+# ## read in xlsx files after the full text screening of all studies 
+# ## (including snowballed studies) and the adjustment of the bibliographic information, 
+# ## do this only once to create an R file.
 # library(readxl)
-# file.list <- 1:27
-# df.list <- lapply(file.list, function(x) read_excel(paste0("study_set_",x,".xlsx")))
 # 
+# # Create a list of file indices from 1 to 27
+# file.list <- 1:27
+# 
+# # Read each Excel file ("study_set_1.xlsx", ..., "study_set_27.xlsx") into a list of data frames
+# df.list <- lapply(file.list, function(x) read_excel(paste0("study_set_", x, ".xlsx")))
 # 
 # library(dplyr)
-# df_full_text <- bind_rows(df.list, .id = "id")
-# # adjust type of doublecheck column
-# df_full_text$doublecheck<-ifelse(df_full_text$doublecheck!=1 | is.na(df_full_text$doublecheck),FALSE,TRUE)
 # 
+# # Combine all data frames in the list into one data frame (df_full_text)
+# df_full_text <- bind_rows(df.list, .id = "id")
+# 
+# # Adjust the type of the 'doublecheck' column to logical
+# df_full_text$doublecheck <- ifelse(df_full_text$doublecheck != 1 | is.na(df_full_text$doublecheck), FALSE, TRUE)
+# 
+# # Filter rows where 'included' is TRUE to only use inluded studies 
+# df_full_text <- df_full_text %>% filter(included == TRUE)
+# 
+# # If 'reason_for_doublecheck' is NA, use the 'reason for doublecheck' column
+# df_full_text$reason_for_doublecheck <- ifelse(is.na(df_full_text$reason_for_doublecheck),
+#                                               df_full_text$`reason for doublecheck`,
+#                                               df_full_text$reason_for_doublecheck)
+# 
+# # Create a new data frame 'doublechecks' containing rows where 'rid2' is not NA, thus containing already doublechecked studies.
+# doublechecks <- df_full_text %>% filter(!is.na(rid2))
+# 
+# # Select specific columns from 'doublechecks' and rename some columns
+# doublechecks <- doublechecks %>% select(key, `publication title`, BibtexKey, rid1, rid2, doublecheck, reason_for_doublecheck)
+# 
+# # Add a new column 'doublecheck_completed' with value 1 since all of theses studies have already been doublechecked.
+# doublechecks$doublecheck_completed <- 1
+# 
+# # Rename columns 6 and 7 in 'doublechecks'
+# colnames(doublechecks)[6:7] <- c("doublecheck_request", "reason_for_doublecheck_request")
+# 
+# # Set the working directory to "~/data/data/doublechecks"
 # setwd("~/data/data/doublechecks")
-# saveRDS(df_full_text,"df_full_text")
-
-setwd("~/data/data/doublechecks")
-
-#load data 
-df_full_text<-readRDS("df_full_text")
-
-
-################## Adapt these three settings:
-
-
-#### Change researcher id accordingly ###########
-researcher<-"me"
-
-#### doublecheck only entries ###########
-doublecheck_only<-FALSE
+# 
+# library(readr)
+# 
+# # Write 'doublechecks' data frame to a CSV file named "assigned_doublechecks.csv"
+# write_csv(doublechecks, "assigned_doublechecks.csv")
+# 
+# # Save the 'df_full_text' data frame as an RDS file
+# saveRDS(df_full_text, "df_full_text")
 
 
-#### Put in the number of studies you want to draw: 
-n_doublecheck<-10
-
-#### round of double check draw per resercher
-round<-1
+##### actually do the doublecheck assignment
 
 
-sum(df_full_text$included, na.rm = TRUE)
+# load package
 
-# draw random sample
-random_sample<-df_full_text %>% filter(rid1!=researcher & is.na(rid2) & doublecheck==doublecheck_only & included==1) %>% # filter to only consider relevant entries
-                                slice_sample(n=n_doublecheck, replace = FALSE) # drawn n observations
+library(MetaExtractR)
+
+setwd("~/data")
+
+# assign doublechecks
+assign_doublechecks("ph",10)
+assign_doublechecks("sg",10)
+assign_doublechecks("me",10)
+assign_doublechecks("fp",10)
+assign_doublechecks("dr",10)
 
 
 
-df_full_text<-setdiff(df_full_text,random_sample)
-
-saveRDS(df_full_text,"df_full_text")
-saveRDS(random_sample,paste0("random_sample_",researcher,round))

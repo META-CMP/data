b7c032aa3b258460486b6a1954c7f428722dae02
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-05-14 16:58:46 +0200

    Update final join

--- data/final_join_testing.R
+++ data/final_join_testing.R
@@ -1,3 +1,5 @@
+library(MetaExtractR)
+
 error_jsons <- c(
   "example_file",
   "V2A8ZH66",# no IRFs
@@ -30,6 +32,9 @@ json.irf.join <- MetaExtractR::final_join(json_path = "data/full_text_screening/
 
 #save(data,file = "preliminary_data.RData")
 
+
+library(dplyr)
+library(tidyverse)
 
 data<-json.irf.join
 
@@ -73,3 +78,44 @@ fun <- function(x, y) {
 }
 
 data$main_research_q<-mapply(fun, data$outcome, data$main)
+
+
+library(zoo)
+
+
+#data %>% filter(end=="Q4–2015")
+
+data$start<-ifelse(data$start=="Q4–2000","Q4-2000",data$start)
+data$end<-ifelse(data$end=="Q4–2015","Q4-2015",data$end)
+
+
+data<-data %>%
+  mutate(observations_calc = case_when(substr(start,1,1)=="Q" ~ ((as.yearqtr(end, format = "Q%q-%Y")-as.yearqtr(start, format = "Q%q-%Y"))*4+1)*as.numeric(n_of_countries),
+                                       substr(start,1,1)!="Q" & grepl("-", start) == TRUE ~ ((as.yearmon(end, format = "%m-%Y")-as.yearmon(start, format = "%m-%Y"))*12+1)*as.numeric(n_of_countries),
+                                       nchar(data$start)==4 ~ (as.numeric(end)-as.numeric(start)+1)*as.numeric(n_of_countries)))
+
+
+data$samplesize<-as.numeric(data$samplesize)
+
+unique(data$samplesize)
+
+data$observations<-ifelse(is.na(data$samplesize), data$observations_calc, as.numeric(data$samplesize))
+
+
+
+
+# set logical values to dummies.
+data<-data %>% mutate_if(is.logical, as.numeric)  
+
+summary(data)
+head(data)
+
+#test<-data %>% filter(observations==1)
+unique(data$study_notes)
+unique(data$model_notes)
+
+data$quality_concern<-grepl("quality_concern",data$study_notes)
+
+data$quality_concern<-ifelse(grepl("quality_concern",data$model_notes),TRUE, data$quality_concern)
+
+

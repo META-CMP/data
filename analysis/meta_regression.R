5df1b3d1669b9c6454af6f9c205a4f6cee4d665d
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-05-28 13:04:58 +0200

    set up regression model and first try BMS package

--- analysis/meta_regression.R
+++ analysis/meta_regression.R
@@ -144,15 +144,41 @@ periods <- c(3, 6, 12, 18, 24, 30, 36, 48)
 # can be deleted later on. Just to recode one NA in the prelimnary dataset which is later set to false. 
 data<-data %>% mutate(prefer=ifelse(is.na(prefer),FALSE,prefer))
 
+# True can be deleted for final dataset as these entries have been recoded. 
 data<-data %>% mutate(dyn_ols=ifelse(dyn_ols=="TRUE" | dyn_ols=="ardl",TRUE,FALSE))
+
+
+split_list<-stri_split_fixed(str = data$inttype, pattern = "_", n = 3)
+
+# Extract nth element from each split list
+data$intrest_rate <- sapply(split_list, function(x) ifelse(3 <= length(x), x[3], NA))
+
+data$interest_rate_short<-ifelse(grepl("3_month",data$intrest_rate) | grepl("1_month",data$intrest_rate) | grepl("6_month",data$intrest_rate) | grepl("1_year",data$intrest_rate)| grepl("2_year",data$intrest_rate) | grepl("ssr",data$intrest_rate) | grepl("shadow_fed_funds",data$intrest_rate)| grepl("long_term_rate",data$intrest_rate)| grepl("shadow_rate",data$intrest_rate),FALSE, TRUE)
+
 
 data<-data %>% unnest(n_of_countries) %>% unnest(chol) %>% unnest(bayes) %>% unnest(dsge) %>% unnest(idother) %>% unnest(var) %>% unnest(model_notes) %>% unnest(prefer) %>% mutate_if(is.logical, as.numeric) 
 summary(data)
 #data %>% unnest(figure)  %>% unnest(page) #data %>% filter(is.na(figure)) is.na figure set to figure Appendix D1.
 
-unique(data$dyn_ols)
+data$us<-as.factor(ifelse(data$list_of_countries=="US","US",ifelse(data$list_of_countries=="EA","EA","other")))
 
-test<-data %>% filter(dyn_ols=="TRUE")
+# create inital shock size measure
+data<-data %>%
+  group_by(key,model_id) %>%
+  mutate(shock_size = rate_mean.effect[period.month == 0])
+
+
+
+equation<-mean.effect_winsor ~standarderror_winsor+exrate+gdppc+cbi+findev+fingl+infl+tradegl+mean_year+regime+quality_concern+observations+main_research_q+real_output+outcome_measure+as.factor(periodicity)+as.factor(transformation)+rate_mean.effect+cbanker+decomposition+convent+pure_rate_shock+lrir+fx+foreignir+inflexp+eglob+find+outpgap+comprice+panel+n_of_countries+us+month+quarter+upr+lor+varother+dsge+bayes+gvar+tvar+fvar+dyn_ols+vecm+lp+idother+longrun+heteroskedas+hf+signr+svar+chol+event+nr+forecast_based+iv+prefer+shock_size+interest_rate_short+as.factor(rid1)+model_id
+
+# +fexch
+
+results_list<-list()
+coef_test_data<-list()
+confint_data<-list()
+
+library(JWileymisc)# to winsorisze data
+library(clubSandwich)# for coef test results
 
 for (x in periods) {
   print(paste("Processing period:", x))
@@ -166,6 +192,7 @@ for (x in periods) {
   # Winsorize data
   data_period_winsor <- data_period
   data_period_winsor$standarderror_winsor <- winsorizor(data_period$StandardError, c(0.02), na.rm = TRUE)
+  data_period_winsor$mean.effect_winsor <- winsorizor(data_period$mean.effect, c(0.02), na.rm = TRUE)
   data_period_winsor$precision_winsor <- 1 / data_period_winsor$standarderror_winsor
   
   # Store the winsorized data
@@ -178,7 +205,7 @@ for (x in periods) {
   data_period_winsor$precvariance_winsor <- 1 / data_period_winsor$variance_winsor
   
   # Calculate (precision-weighted) average
-  regwa <- lm(mean.effect_winsor ~standarderror_winsor, data = data_period_winsor)#, weights = precvariance_winsor
+  regwa <- lm(equation, data = data_period_winsor)#, weights = precvariance_winsor
   results_list[[paste0(x, ".ols")]] <- regwa
   
   coef_test_data[[paste0(x, ".ols")]]<-coef_test(regwa, vcov = "CR0", 
@@ -188,5 +215,43 @@ for (x in periods) {
   confint_data[[paste0(x, ".ols")]]<-confint(regwa, level=0.95)
 }
 
+
+
+dataspeed<-data_period_winsor %>% ungroup() %>% select(mean.effect_winsor, standarderror_winsor, exrate, gdppc, cbi, findev, fingl, infl, tradegl, mean_year, regime, quality_concern, observations, main_research_q, real_output, outcome_measure, periodicity,transformation, rate_mean.effect, cbanker, decomposition, convent, pure_rate_shock, lrir, fx, foreignir, inflexp, eglob, find, outpgap, comprice, panel, n_of_countries, us, month, quarter, upr, lor, varother, dsge, bayes, gvar, tvar, fvar, dyn_ols, vecm, lp, idother, longrun, heteroskedas, hf, signr, svar, chol, event, nr, forecast_based, iv, prefer, shock_size, interest_rate_short,rid1, model_id)
+
+library(fastDummies)
+
+dataspeed<-fastDummies::dummy_cols(dataspeed) %>% select(-rid1,-us,-transformation,-periodicity,-outcome_measure,-rid1_me,-us_other, -transformation_log,-periodicity_a,-outcome_measure_ip)
+
+
+dataspeed %>% 
+  summarise(across(everything(), ~ sum(is.infinite(.x))))
+
+dataspeed<-na.omit(dataspeed)
+
+#dataspeed<-dataspeed[,1:10]
+
+
+library(car)
+
+
+# Calculate VIF for each variable
+vif_values <- car::vif(lm(dataspeed))
+test<-cor(dataspeed)
+# Print the VIF values
+print(vif_values)
+
+
+#//Starts estimation
+library(BMS)
+speed <- bms(dataspeed, g="UIP", mprior="uniform", user.int=FALSE,nmodel = 1000, mcmc="bd")
+
+
+library(modelsummary)
+#modelsummary(results_list, output = "gt",vcov = 'cr0', cluster = data_period_winsor$key,stars = TRUE) # only works if cluster variable has the same number of observations across lists. 
+modelsummary(results_list, output = "gt",stars = TRUE)
+
+coef_test_data<-data.table::rbindlist(coef_test_data, fill = T,idcol = ".id")
+coef_test_data
 
 

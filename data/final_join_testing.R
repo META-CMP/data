1fcd7e9d4875a979e50cab1a00eb40dd6e16efec
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-06-20 09:42:17 +0200

    adjust folder names DEG8EDUV

--- data/final_join_testing.R
+++ data/final_join_testing.R
@@ -12,5 +12,6 @@ json.irf.join <- final_join(json_path = "data/full_text_screening/JSON_files", i
 data<-json.irf.join
 
 save(data,file = "preliminary_data.RData")
+save(data,file = "data/preliminary_data_18062024.RData")
 
 

650f29195f283c26697bd2a1e2fd2c474e1c254c
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-06-24 15:30:59 +0200

    update dataset

--- data/final_join_testing.R
+++ data/final_join_testing.R
@@ -3,8 +3,7 @@ library(MetaExtractR)
 error_jsons <- c(
   "example_file",
   "V2A8ZH66",# no IRFs
-  "Q5FHEZNE", # contains table estimates as well. 
-  "NADVD2AD" # Error: File '/Users/franzprante/GitHub/MORPEP/META_CMP/data/data/effect_sizes/IRFs/NADVD2AD/model_1/log_q_rgdp/mean.csv' does not exist or is non-readable. getwd()=='/Users/franzprante/GitHub/MORPEP/META_CMP/data'
+  "Q5FHEZNE" # contains table estimates as well. 
 )
 
 json.irf.join <- final_join(json_path = "data/full_text_screening/JSON_files", irf_path = "data/effect_sizes/IRFs/", only_json = FALSE, ignore = error_jsons)

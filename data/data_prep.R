92441e21f88f51127a96f1429a707671b9e593b8
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-07-03 16:56:45 +0200

    update data and add top tier dummy

--- data/data_prep.R
+++ data/data_prep.R
@@ -4,7 +4,7 @@ gc() #free up memory and report the memory usage.
 
 #setwd and load dataset
 # setwd("~/data")
-load("data/preliminary_data_20062024.RData")
+load("data/preliminary_data_03072024.RData")
 
 
 # load packackages 

cea60169a483379d88d1bb104b769aff926ee13c
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-07-11 15:20:56 +0200

    update bms output

--- analysis/BMS.R
+++ analysis/BMS.R
@@ -22,7 +22,7 @@ library(BMS)# BMS package for bayesian model averaging.
 data<-data_back
 
 # subset data for outcome variable of interest. 
-outvar<-'inflation'#c("gdp", "inflation", "unemp", "emp")
+outvar<-'gdp'#c("gdp", "inflation", "unemp", "emp")
 data <- subset(data, outcome %in% outvar)
 
 #x<-3
@@ -30,6 +30,7 @@ data <- subset(data, outcome %in% outvar)
 periods <- c(3, 6, 12, 18, 24, 30, 36, 48)
 results_list <- list()
 vif_list <- list()
+bms_full_list <- list()
 
 for (x in periods) {
 
@@ -77,6 +78,7 @@ data_bms<-as.data.frame(data_bms)
 #BMS estimation
 speed <- bms(data_bms, g="UIP", mprior="uniform", user.int=FALSE,nmodel = 20000,burn = 5000, iter=10000, mcmc="bd")#,start.value = c(1:35)
 
+bms_full_list[[paste0(x)]] <- speed
 
 #save results in results list BMS
 results_list[[paste0(x)]] <- coef(speed, order.by.pip = T, exact=T, include.constant=T)
@@ -94,28 +96,113 @@ results_list[[paste0(x)]] <- coef(speed, order.by.pip = T, exact=T, include.cons
 ### image(speed,yprop2pip=TRUE,col=c("black","lightgrey"))
 }
 
-ti <- data.frame(
-  term = names(vif_list[[1]]),
-  estimate = unname(vif_list[[1]]),
-  std.error = c(pi, exp(1), sqrt(2)))
-
-gl <- data.frame(
-  stat1 = "blah",
-  stat2 = "blah blah")
-
-mod <- list(
-  tidy = ti,
-  glance = gl)
-class(mod) <- "modelsummary_list"
-
-modelsummary(mod)
-unname(vif_list[[1]])
-1:3
-parameters::parameters(speed)
-modelsummary(as.data.frame(coef(speed)), output = "gt",stars = TRUE)
+########## Print vif values
 
 
-# plot(speed)
-plotConv(speed)
-# image(speed, cex.axis=.5, order.by.pip = T, yprop2pip=F)#,col=c("orange","lightgrey"), main=paste0("BMA image ",outcome," ",x," months")
-#density(speed, reg="dsge")
+# function to map vif_list to modelsummary_list element
+create_modelsummary_list_vif <- function(vif_element) {
+  ti <- data.frame(
+    term = names(vif_element),
+    estimate = unname(vif_element),
+    std.error = rep(NA, length(vif_element))
+  )
+  
+  gl <- data.frame(
+    stat1 = "VIF values",
+    stat2 = "NA"
+  )
+  
+  mod <- list(
+    tidy = ti,
+    glance = gl
+  )
+  
+  class(mod) <- "modelsummary_list"
+  return(mod)
+}
+
+# Apply the function to each element in vif_list
+mod_list <- lapply(vif_list, create_modelsummary_list_vif)
+
+# show vif factors for all time periods
+modelsummary(mod_list)
+
+
+########## Print BMS results
+create_modelsummary_list_bms_pip <- function(result_element) {
+  ti <- data.frame(
+    term = names(result_element[,1]),
+    estimate = paste0("PIP: ",round(unname(result_element[,1]),2)),
+    std.error = paste0("Sign: ",round(unname(result_element[,4]),2))
+  )
+  
+  gl <- data.frame(
+    stat1 = "VIF values",
+    stat2 = "NA"
+  )
+  
+  mod <- list(
+    tidy = ti,
+    glance = gl
+  )
+  
+  class(mod) <- "modelsummary_list"
+  return(mod)
+}
+
+create_modelsummary_list_bms_coef <- function(result_element,bms_element) {
+  ti <- data.frame(
+    term = paste0(names(result_element[,1])),
+    estimate = paste0("[",round(unname(result_element[,1]),2),"] \n",format(round(unname(result_element[,2]),4),nsmall=2)),
+    std.error = unname(result_element[,3])
+  )
+  
+  gl <- data.frame(
+    K = bms_element[[1]]$K,
+    N = bms_element[[1]]$N,
+    Models.visited = bms_element[[1]]$models.visited,
+    burn=bms_element[[1]]$burn,
+    iter=bms_element[[1]]$iter,
+    msize=bms_element[[1]]$msize
+  )
+  
+  mod <- list(
+    tidy = ti,
+    glance = gl
+  )
+  
+  class(mod) <- "modelsummary_list"
+  return(mod)
+}
+
+
+
+#mod_list_coef <- lapply(results_list, create_modelsummary_list_bms_coef)
+
+mod_list_coef <- Map(create_modelsummary_list_bms_coef,results_list,bms_full_list)
+mod_list_pip<- lapply(results_list, create_modelsummary_list_bms_pip)
+
+
+# show vif factors for all time periods
+modelsummary(mod_list_coef)
+modelsummary(mod_list_pip)
+#modelsummary(mod)
+
+
+
+# ti <- data.frame(
+#   term = names(results_list[[1]][,1]),
+#   estimate = paste0("PIP: ",round(unname(results_list[[1]][,1]),2)," - Mean: ",round(unname(results_list[[1]][,2]),3)),
+#   std.error = paste0("Sign: ",round(unname(results_list[[1]][,4]),2)," - SE: ",round(unname(results_list[[1]][,3]),3))
+# 
+
+test<-bms_full_list[[1]][[1]]$models.visited
+
+element<-4
+
+plot(bms_full_list[[element]])
+plotConv(bms_full_list[[element]])
+image(bms_full_list[[element]], cex.axis=.5, order.by.pip = T, yprop2pip=F)#,col=c("orange","lightgrey"), main=paste0("BMA image ",outcome," ",x," months")
+density(bms_full_list[[element]], reg="dsge")
+
+

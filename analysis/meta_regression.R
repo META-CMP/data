ada7aa5a44e0b61bb90470f4c30053fa3cda3b99
MEnzi93 <45854298+MEnzi93@users.noreply.github.com>
2024-07-31 11:19:30 +0200

    add external data regression

--- analysis/meta_regression.R
+++ analysis/meta_regression.R
@@ -39,8 +39,19 @@ equation<-mean.effect_winsor ~standarderror_winsor+group_ident_broad+lp+vecm+dyn
 
 # currently not included: rate_mean.effect (due to missing data),country_dev, conf, intrest_rate, external control variables. (periodicity does not really make sense the way it currently looks, we would need to replace the log a values) (real output does not make sense currently, we would need to check the non real ones again.) (initial shock size needs to be adjusted - size)
 
-small<-mean.effect_winsor ~standarderror_winsor+group_ident_broad+lp+vecm+dyn_ols+fvar+tvar+gvar+dsge+varother+panel+bayes+regime+upr+lor+hike+cut+decomposition+convent+cbanker+is_top_tier+is_top_5+main_research_q+prefer
+small<-mean.effect_winsor ~standarderror_winsor+group_ident_broad+lp+vecm+dyn_ols+fvar+tvar+gvar+dsge+varother+panel+bayes+regime+upr+lor+hike+cut+decomposition+convent+cbanker+is_top_tier+is_top_5+main_research_q
 #colSums(is.na(data_period_winsor %>% select(standarderror_winsor,group_ident_broad,lp,vecm,dyn_ols,fvar,tvar,gvar,dsge,varother,cbanker,is_top_tier,is_top_5)))
+
+external<- mean.effect_winsor ~standarderror_winsor+tradegl+log(infl)+fingl+findev+cbi+log(gdppc)+exrate
+
+
+data %>% select(key,model_id,tradegl:exrate) %>% group_by(key,model_id) %>% summarise(across(tradegl:exrate, ~ mean(.x, na.rm = TRUE))) %>% mutate(infl=log(infl),gdppc=log(gdppc)) %>% 
+  pivot_longer(cols = tradegl:exrate, names_to = "variable", values_to = "value") %>% ggplot( aes(x = value)) +
+  geom_density(fill = "blue", alpha = 0.5) +
+  facet_wrap(~ variable, scales = "free") +
+  labs(title = "Density Plots for All Variables", x = "Value", y = "Density") +
+  theme_minimal()
+
 
 # +fexch#+real_output # only for output regression
 
@@ -130,59 +141,3 @@ coef_test_data
 
 plot(density(2024-data$`publication year`))
 
-######################## search for explaining factors of high precision: 
-
-#equation<-precvariance_winsor ~mean_year+regime+quality_concern+observations+main_research_q+outcome_measure+as.factor(periodicity)+as.factor(transformation)+cbanker+decomposition+convent+pure_rate_shock+lrir+fx+foreignir+inflexp+eglob+find+outpgap+comprice+panel+n_of_countries+us+month+quarter+upr+lor+varother+dsge+bayes+gvar+tvar+fvar+dyn_ols+vecm+lp+idother+longrun+heteroskedas+hf+signr+svar+chol+event+nr+forecast_based+iv+prefer+interest_rate_short+as.factor(rid1)+model_id
-
-equation<-precvariance_winsor ~mean_year+quality_concern+observations+main_research_q+relevel(as.factor(transformation), ref = "log")+cbanker+panel+us+month+dsge+lp+signr+prefer
-# +fexch#+real_output # only for output regression
-
-periods <- c(3, 6, 12, 18, 24, 30, 36, 48)
-results_list<-list()
-vif_list<-list()
-for (x in periods) {
-  print(paste("Processing period:", x))
-  
-  # Subset data for the current period
-  data_period <- subset(data, period.month %in% x)
-
-
-data_period$StandardError <- (data_period$SE.upper + data_period$SE.lower) / 2
-data_period$precision <- 1 / data_period$StandardError
-
-# Winsorize data
-data_period_winsor <- data_period
-data_period_winsor$standarderror_winsor <- winsorizor(data_period$StandardError, c(0.0002), na.rm = TRUE)
-data_period_winsor$mean.effect_winsor <- winsorizor(data_period$mean.effect, c(0.0002), na.rm = TRUE)
-data_period_winsor$precision_winsor <- 1 / data_period_winsor$standarderror_winsor
-
-# Store the winsorized data
-
-
-# Calculate variance winsorised
-data_period_winsor$variance_winsor <- data_period_winsor$standarderror_winsor^2
-
-# Calculate PrecVariance winsorised
-data_period_winsor$precvariance_winsor <- 1 / data_period_winsor$variance_winsor
-
-dataspeed<-data_period_winsor %>% ungroup() %>% select(precvariance_winsor, mean_year,quality_concern,observations,main_research_q,transformation,cbanker,panel,us,month,dsge,lp,signr,prefer)
-
-anyNA(dataspeed)
-
-test<-dataspeed %>% 
-  summarise(across(everything(), ~ sum(is.na(.x))))
-
-dataspeed<-na.omit(dataspeed)
-
-vif_values <- car::vif(lm(dataspeed))
-vif_list[[paste0(x, ".vif")]] <- vif_values
-# Calculate (precision-weighted) average
-regwa <- lm(equation, data = data_period_winsor)#, weights = precvariance_winsor
-both_model <- step(regwa, direction = "both")
-results_list[[paste0(x, ".ols")]] <- both_model
-}
-
-
-modelsummary(results_list, output = "gt",stars = TRUE)
-
-vif_list
